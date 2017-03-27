baa833984bcb1dd882fed6fba21fc02b8a461b0e
Josh Hollandsworth <jshollandsworth@gmail.com>
2017-03-27 15:24:57 -0400

    Added some new blocks to streamfield to build pages: rotating banner with links, variations of the column highlight blocks. Updated block templates for column highlights as well.

--- website/home/custom_blocks/my_blocks.py
+++ website/home/custom_blocks/my_blocks.py
@@ -10,17 +10,17 @@ from wagtail.wagtailadmin.edit_handlers import FieldPanel, MultiFieldPanel, \
 
 
 class ThreeColumnHighlightBlock(blocks.StructBlock):
-	left_column = blocks.StreamBlock([
+	left_column = blocks.StructBlock([
 		('heading', blocks.CharBlock(classname="full title")),
 		('paragraph', blocks.RichTextBlock()),
 		], icon='arrow-left', label='Left column content')
 
-	center_column = blocks.StreamBlock([
+	center_column = blocks.StructBlock([
 		('heading', blocks.CharBlock(classname="full title")),
 		('paragraph', blocks.RichTextBlock()),
 		], icon='arrow-down', label='Right column content')
 
-	right_column = blocks.StreamBlock([
+	right_column = blocks.StructBlock([
 		('heading', blocks.CharBlock(classname="full title")),
 		('paragraph', blocks.RichTextBlock()),
 		], icon='arrow-right', label='Right column content')
@@ -31,14 +31,43 @@ class ThreeColumnHighlightBlock(blocks.StructBlock):
 		label = 'Three Column Highlight'
 
 
+class TwoColumnHighlightBlock(blocks.StructBlock):
+	left_column = blocks.StructBlock([
+		('heading', blocks.CharBlock(classname="full title")),
+		('paragraph', blocks.RichTextBlock()),
+		], icon='arrow-left', label='Left column content')
+
+	right_column = blocks.StructBlock([
+		('heading', blocks.CharBlock(classname="full title")),
+		('paragraph', blocks.RichTextBlock()),
+		], icon='arrow-right', label='Right column content')
+
+	class Meta:
+		template = 'home/blocks/two_column_highlight_block.html'
+		icon = 'pick'
+		label = 'Two Column Highlight'
+
+
+class OneColumnHighlightBlock(blocks.StructBlock):
+	column = blocks.StructBlock([
+		('heading', blocks.CharBlock(classname="full title")),
+		('paragraph', blocks.RichTextBlock()),
+		], icon='arrow-left', label='Left column content')
+
+	class Meta:
+		template = 'home/blocks/one_column_highlight_block.html'
+		icon = 'pick'
+		label = 'One Column Highlight'
+
+
 class SingleImageHeader(blocks.StructBlock):
 	header = blocks.StreamBlock([
 		('header_image', ImageChooserBlock(required=True)),
-	    ('header_top_text', blocks.CharBlock(required=True)),
-	    ('header_sub_text', blocks.CharBlock()),
-	    ('header_info_text', blocks.CharBlock()),
-	    ('header_cta', blocks.CharBlock(required=True)),
-	    ('header_target_link', blocks.PageChooserBlock(required=True)),
+		('header_top_text', blocks.CharBlock(required=True)),
+		('header_sub_text', blocks.CharBlock()),
+		('header_info_text', blocks.CharBlock()),
+		('header_cta', blocks.CharBlock(required=True)),
+		('header_target_link', blocks.PageChooserBlock(required=True)),
 		], icon = "image", label = "Single Image Header")
 
 	class Meta:
@@ -47,5 +76,30 @@ class SingleImageHeader(blocks.StructBlock):
 		label = 'Single Image Header'
 
 
-# class RotatingImageHeader(blocks.StructBlock):
-# 	pass
+class HeaderLinkBlock(blocks.StructBlock):
+	cta = blocks.CharBlock(required=True)
+	url_link = blocks.PageChooserBlock(required=True)
+	
+	class Meta:
+		icon = "image"
+		label = "Header Link Block"
+
+
+class RotatingImageBlock(blocks.StructBlock):
+	text = blocks.StructBlock([
+		('header_top_text', blocks.CharBlock(required=True)),
+		('header_sub_text', blocks.CharBlock()),
+		('header_info_text', blocks.CharBlock()),
+		], icon = "image", label = "Rotating Image Banner")
+
+	links = blocks.ListBlock(HeaderLinkBlock())
+
+	images = blocks.ListBlock(ImageChooserBlock(required=True))
+
+	class Meta:
+		template = 'home/blocks/rotating_image_block.html'
+		icon = 'image'
+		label = 'Rotating Image Header'
+
+
+
--- website/home/models.py
+++ website/home/models.py
@@ -11,34 +11,21 @@ from wagtail.wagtailimages.edit_handlers import ImageChooserPanel
 from wagtail.wagtailimages.blocks import ImageChooserBlock
 from wagtail.wagtailsnippets.edit_handlers import SnippetChooserPanel
 
-from .custom_blocks.my_blocks import ThreeColumnHighlightBlock, SingleImageHeader
+from .custom_blocks.my_blocks import OneColumnHighlightBlock, TwoColumnHighlightBlock, ThreeColumnHighlightBlock,\
+                                     SingleImageHeader, RotatingImageBlock
 
 
 class IndexPage(Page):
     header = StreamField([
-        ('single_image_header', SingleImageHeader())
+        ('single_image_header', SingleImageHeader()),
+        ('rotating_image_header', RotatingImageBlock()),
     ])
     body = StreamField([
-        ('three_column_highlight', ThreeColumnHighlightBlock())
+        ('one_column_highlight', OneColumnHighlightBlock()),
+        ('two_column_highlight', TwoColumnHighlightBlock()),
+        ('three_column_highlight', ThreeColumnHighlightBlock()),
+        
     ])
-    # header_image = models.ForeignKey(
-    #     'wagtailimages.Image',
-    #     null=True,
-    #     blank=True,
-    #     on_delete=models.SET_NULL,
-    #     related_name='+'
-    #     )   
-    # header_top_text = models.CharField(max_length=255)
-    # header_sub_text = models.CharField(max_length=255)
-    # header_info_text = models.TextField()
-    # header_cta = models.CharField(max_length=60)
-    # header_target_link = models.ForeignKey(
-    #     'wagtailcore.Page',
-    #     null=True,
-    #     blank=True,
-    #     on_delete=models.SET_NULL,
-    #     related_name='+',
-    # )
 
     content_panels = Page.content_panels + [
     	StreamFieldPanel('header'),
